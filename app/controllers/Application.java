c5b55107415edb20fc3f22221cb0521c259ecd16
leonsulivan <contact@jerome-baudoux.com>
2014-09-11 17:36:02 +0200

    dev/prod handling

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,9 +3,28 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 public class Application extends Controller {
 	
 	public static Result index() {
 		return ok("Work in progress");
 	}
+	
+    public static Result dev(String name) {
+        String fileName;
+        try {
+            fileName = URLDecoder.decode(name, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            fileName = name;
+        }
+        File file = new File("ui/app/" + fileName);
+        try {
+			return ok(file, true);
+		} catch(Throwable t) {
+			return notFound();
+		}
+    }
 }
