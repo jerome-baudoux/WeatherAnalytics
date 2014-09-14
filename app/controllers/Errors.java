a5d5ad487de42cad11090066ab8c530b170e8fe5
Jerome Baudoux <contact@jerome-baudoux.com>
2014-09-14 10:27:37 +0200

    Errors Enhancements

--- app/controllers/Errors.java
+++ app/controllers/Errors.java
@@ -3,32 +3,37 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-import views.html.*;
-
 /**
  * Handle errors on the back end side
  * @author Jerome Baudoux
  */
 public class Errors extends Controller {
 
-	/**
-	 * @return show error page
-	 */
-	public static Result unknown() {
-		return ok(errors.render("Internal Error", "Something went wrong... That's all we know..."));
-	}
+	public static String TITLE_ERROR_404 = "Error 404";
+	public static String TITLE_ERROR_500 = "Internal Error";
+
+	public static String MESSAGE_ERROR_404 = "File not found";
+	public static String MESSAGE_ERROR_500_WITH_TEXT = "Something went wrong: ";
+	public static String MESSAGE_ERROR_500 = "Something went wrong... That's all we know...";
 
 	/**
 	 * @return show error page
 	 */
 	public static Result missingFile() {
-		return ok(errors.render("Error 404", "File not found"));
+		return notFound(views.html.errors.render(TITLE_ERROR_404, MESSAGE_ERROR_404));
 	}
 
 	/**
 	 * @return show error page
 	 */
 	public static Result message(String message) {
-		return ok(errors.render("Internal Error", "Something went wrong: " + message));
+		return internalServerError(views.html.errors.render(MESSAGE_ERROR_500, MESSAGE_ERROR_500_WITH_TEXT + message));
+	}
+	
+	/**
+	 * @return show error page
+	 */
+	public static Result unknown() {
+		return internalServerError(views.html.errors.render(TITLE_ERROR_500, MESSAGE_ERROR_500));
 	}
 }
---
+++ app/controllers/PublicAssets.scala
@@ -0,0 +1,33 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+/**
+ * Controller that handles fetching elements from the public assets
+ */
+object PublicAssets extends Controller {
+
+  /**
+   * Fetch the production file.
+   * If an error occurs, show a customized error page
+   */
+  def at(path:String, file:String) = Action.async { implicit request =>
+    Assets.at(path, file, false).apply(request).map(result => {
+			// No error just send the result
+			if(result.header.status < 400) {
+				result
+			} 
+			// Specific 404 error
+			else if(result.header.status == 404) {
+				NotFound(views.html.errors(Errors.TITLE_ERROR_404)(Errors.MESSAGE_ERROR_404))
+			}
+			// Other kind of errors
+			else {
+				InternalServerError(views.html.errors(Errors.TITLE_ERROR_500)(Errors.MESSAGE_ERROR_500))
+			}
+		}
+    )
+  }
+}
