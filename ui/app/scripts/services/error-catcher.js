91e2bf1076006b888f0e7ddc776fb501d0d415a1
Jerome Baudoux <contact@jerome-baudoux.com>
2014-09-19 21:51:37 +0200

    Use minified version of error-logger + prevent the app from failing if the dependency is missing

--- ui/app/scripts/services/error-catcher.js
+++ ui/app/scripts/services/error-catcher.js
@@ -11,14 +11,48 @@
 angular.module('uiApp')
     .factory('$exceptionHandler', function () {
         return function errorCatcherHandler(exception) {
+		
+			// Write in the console ASAP
             try {
                 console.error('Uncatched exception' + exception.stack);
             } catch (e) {
                 console.error('Uncatched exception' + exception);
             }
-            ErrorLogger.getInstance().handleError('Uncatched exception', exception, true);
+		
+			// Do not break if the class is not found
+            try {
+				ErrorLogger.getInstance().handleError('Uncatched exception', exception, true);
+			} catch(e) {
+                console.error('Cannot log error message: ' + e.stack);
+			}
         };
     })
     .service('exceptionLogger', function() {
-        return ErrorLogger.getInstance();
+		var logger;
+		
+		// Do not break if the class is not found
+		try {
+			logger = ErrorLogger.getInstance();
+		} catch(e) {
+			console.error('Cannot load error logger: ' + e.stack);
+		}
+		
+		// Do not send the object, use an interface
+		return {
+			handleError: function (message, error, doNotShowInConsole) {
+				if(logger) {
+					logger.handleError(message, error, doNotShowInConsole);
+				}
+			},
+			getLogs: function() {
+				if(logger) {
+					return logger.getLogs();
+				}
+			},
+			clearLogs: function() {
+				if(logger) {
+					return logger.clearLogs();
+				}
+			}
+		};
     });
